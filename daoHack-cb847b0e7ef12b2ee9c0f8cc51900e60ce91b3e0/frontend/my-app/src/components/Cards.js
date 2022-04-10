80c8439e55ff8507e69a368f97f702bd0d717d00
ZiedChekir <ziedchekir1@gmail.com>
2022-04-10 01:11:55 +0100

    cards working

--- daoHack-cb847b0e7ef12b2ee9c0f8cc51900e60ce91b3e0/frontend/my-app/src/components/Cards.js
+++ daoHack-cb847b0e7ef12b2ee9c0f8cc51900e60ce91b3e0/frontend/my-app/src/components/Cards.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
 import axios from 'axios';
@@ -6,99 +6,78 @@ import { Web3Storage } from 'web3.storage';
 
 
 
-function getAccessToken() {
-  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEU0RDU1OENjNGEyZGU4ODg2MGU0M2JkMDhGNDM3Y2NmMDRGN0Y5Q2IiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDk1MDIyMTQwNjAsIm5hbWUiOiJ0ZXN0In0.maFSn8Y-xBvN8UQhnb_44NHZRVLu90u-E-R-4u089es'
-}
-function makeStorageClient() {
-  return new Web3Storage({ token: getAccessToken() })
-}
-let Object = [];
-async function listUploads() {
-  const client = makeStorageClient()
-  for await (const upload of client.list()) {
-    Object.push(retrieveFilesHTTP(upload.cid,upload.name));
-   //console.log( retrieveFilesHTTP(upload.cid,upload.name));
-  }
-  return Object;
-}
+function CardsContainer(props) {
+console.log(props.data)
 
-async function retrieveFilesHTTP(cid) {
- let FileName = "Project.json"
-  const a = await axios.get("https://" + cid + ".ipfs.dweb.link/" + FileName);
-   return a.data
-}
-
-function Cards() {
-  let objects = Object;
-
-  return (
+  return (<div>
     <div className='cards'>
       <h1>Check out these EPIC Projects!</h1>
       <div className='card-columns'>
         <div className='cards__wrapper'>
+
           <ul className='cards__items'>
-          {
-            objects.map(function(object, i){
-              return <CardItem
-              src='/images/peepohands.jpeg'
-              text='RobinMania'
-              label= {object}
-              path='/show_proj/'
-            />;
-          })
-          }
+            {props.data.map((x) => (
+              <CardItem
+                src={"https://" + x.cid + ".ipfs.dweb.link/Image.png" }
+                text={x.title}
+                label='cute'
+                path="/show_proj/"
+                id={x.WalletAdress}
+              />
+            ))}
+
           </ul>
-          <ul className='cards__items'>
-            <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path="/show_proj/"
-              id='1'
-            />
-             <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path='/Project/'
-              id='2'
-            />
-             <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path='/services'
-              id='3'
-            />
-            </ul>
-            <ul className='cards__items'>
-             <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path='/services'
-              id='4'
-            />
-             <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path='/services'
-              id='5'
-            />
-             <CardItem
-              src='/images/peeposhy.jpg'
-              text='A shy peepo'
-              label='cute'
-              path='/services'
-              id='6'
-            />
-          </ul>
-        
+
         </div>
       </div>
     </div>
+
+  </div>)
+}
+
+function Cards() {
+  const [data, setData] = useState([]);
+  function getAccessToken() {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweEU0RDU1OENjNGEyZGU4ODg2MGU0M2JkMDhGNDM3Y2NmMDRGN0Y5Q2IiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NDk1MDIyMTQwNjAsIm5hbWUiOiJ0ZXN0In0.maFSn8Y-xBvN8UQhnb_44NHZRVLu90u-E-R-4u089es'
+  }
+  function makeStorageClient() {
+    return new Web3Storage({ token: getAccessToken() })
+  }
+  async function listUploads() {
+    const client = makeStorageClient()
+    for await (const upload of client.list()) {
+      //Object.push(retrieveFilesHTTP(upload.cid,upload.name));
+      retrieveFilesHTTP(upload.cid);
+    }
+
+  }
+  async function retrieveFilesHTTP(cid) {
+    let FileName = "Project.json"
+    const a = await axios.get("https://" + cid + ".ipfs.dweb.link/" + FileName);
+    let newData = { title: a.data.title, description: a.data.description, walletAdress: a.data.walletAdress, cid: cid }
+    return newData
+  }
+
+  useEffect(async () => {
+    async function test() {
+      let arrayy = [];
+      const client = makeStorageClient()
+      for await (const upload of client.list()) {
+        //Object.push(retrieveFilesHTTP(upload.cid,upload.name));
+        let FileName = "Project.json"
+        const x = await axios.get("https://" + upload.cid + ".ipfs.dweb.link/" + FileName)
+        let newData = { title: x.data.title, description: x.data.Description, walletAdress: x.data.WalletAdress, cid: upload.cid }
+        arrayy.push(newData);
+      }
+      setData(arrayy);
+    }
+    test()
+  }, []);
+
+  return (
+    <CardsContainer data={data} />
   );
+
 }
 
 export default Cards;
