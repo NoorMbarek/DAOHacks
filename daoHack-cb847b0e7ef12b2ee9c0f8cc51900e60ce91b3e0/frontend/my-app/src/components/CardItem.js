b107b36ee18ce3886dfc1363030756c62405c847
Alaa Souabni <alaa.souabni@supcom.tn>
2022-04-10 01:26:20 +0100

    cards new

--- daoHack-cb847b0e7ef12b2ee9c0f8cc51900e60ce91b3e0/frontend/my-app/src/components/CardItem.js
+++ daoHack-cb847b0e7ef12b2ee9c0f8cc51900e60ce91b3e0/frontend/my-app/src/components/CardItem.js
@@ -9,19 +9,29 @@ function CardItem(props) {
   return (
     <>
       <li className='cards__item'>
+        
         <Link className='cards__item__link' to={props.path+props.id}>
           <figure className='cards__item__pic-wrap' data-category={props.label}>
+            
             <img
               className='cards__item__img'
               alt='Travel Image'
               src={props.src}
             />
           </figure>
+          
           <div className='cards__item__info'>
+            <h3>Title</h3>
+            <h4>Address</h4>
             <h5 className='cards__item__text'>{props.text}</h5>
+            <p align='center'><button className="invBtn">Investment Proposal</button></p>
           </div>
+          
         </Link>
+        
+        
       </li>
+      
     </>
   );
 }
